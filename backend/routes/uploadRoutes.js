ead15c49f639edf119b6b5be979ccb3feee2ff17
MauricioAlonso <program.alonso@gmail.com>
2024-07-01 17:19:57 -0300

    probando

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,3 +1,61 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/"); // Directorio donde se guardarán los archivos subidos
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const filetypes = /jpeg|jpg|png|webp/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    cb(null, true);
+  } else {
+    cb(new Error("Solo se permiten imágenes con extensiones jpeg, jpg, png o webp"));
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single("image");
+
+router.post("/", (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // Un error de multer ocurrió durante la carga
+      res
+        .status(400)
+        .json({ message: "Error al subir la imagen", error: err.message });
+    } else if (err) {
+      // Otro tipo de error ocurrió
+      res
+        .status(500)
+        .json({ message: "Error interno del servidor", error: err.message });
+    } else {
+      // La imagen se subió correctamente
+      res.status(200).json({
+        message: "Imagen subida correctamente",
+        image: `/${req.file.path}`, // Ruta relativa para acceder a la imagen
+      });
+    }
+  });
+});
+
+export default router;
+
+/*
 import path from 'path'
 import express from 'express'
 import multer from 'multer'
@@ -43,3 +101,4 @@ router.post("/", (req, res) => {
 });
 
 export default router
+*/
