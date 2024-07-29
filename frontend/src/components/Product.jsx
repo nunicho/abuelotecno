838d85dd6d8cc2430dda811ffd56511445574f54
MauricioAlonso <program.alonso@gmail.com>
2024-07-28 22:34:31 -0300

    listo

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -5,6 +5,68 @@ import Rating from "./Rating";
 import "../assets/styles/productCard.css"; // Asegúrate de importar tu archivo CSS
 
 const Product = ({ product }) => {
+  const formattedPrice = (price) => price.toFixed(2);
+
+  return (
+    <Card className="my-3 p-3 rounded custom-card">
+      {product.discountPrice && <div className="promo-badge">PROMO</div>}
+      <Link to={`/product/${product._id}`}>
+        <Card.Img
+          src={product.image}
+          variant="top"
+          className="custom-card-img"
+        />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div" className="product-title custom-card-title">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            className="custom-rating"
+          />
+        </Card.Text>
+
+        <Card.Text
+          as="div"
+          className={`price-container ${
+            product.discountPrice ? "has-discount" : ""
+          }`}
+        >
+          {product.discountPrice ? (
+            <>
+              <span className="original-price">
+                ${formattedPrice(product.price)}
+              </span>
+              <span className="discount-price">
+                ${formattedPrice(product.discountPrice)}
+              </span>
+            </>
+          ) : (
+            <span className="custom-price">
+              ${formattedPrice(product.price)}
+            </span>
+          )}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
+/*
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import "../assets/styles/productCard.css"; // Asegúrate de importar tu archivo CSS
+
+const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded custom-card">
       <Link to={`/product/${product._id}`}>
@@ -38,39 +100,4 @@ const Product = ({ product }) => {
 };
 
 export default Product;
-
-/*
-import React from 'react'
-
-import Card from "react-bootstrap/Card"
-import {Link} from "react-router-dom"
-import Rating from "./Rating"
-
-
-const Product = ({product}) => {
-  return (
-    <Card className="my-3 p-3 rounded">
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant="top" />
-        </Link>
-        <Card.Body>
-            <Link to={`/product/${product._id}`}>
-                <Card.Title as="div" className='product-title'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </Link>
-            
-            <Card.Text as="div">
-                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-            </Card.Text>
-
-            <Card.Text as="h3">
-                ${product.price}
-            </Card.Text>
-        </Card.Body>
-    </Card>
-  )
-}
-
-export default Product
 */
